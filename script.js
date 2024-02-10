36244e5715279bb552453270abb2977a638ff578
SrormDevXK <mhstudio1912@gmail.com>
2024-02-10 22:29:38 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict"
+let monthNames= ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"]
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
+}
+function startOfMonth(date) {
+    return new Date(date.getFullYear(), date.getMonth(), 1).getDay()
+}
+
+function monthArrayGenerator(date){
+    let monthArr = [[]]
+    let dayCount = 1
+    for(let w = 0; w < 5; w++){
+        if(w === 0){
+            let preDays = daysInMonth(date.getMonth(), date.getFullYear())
+            for(let i = preDays - startOfMonth(date)+2; i <= preDays; i++){monthArr[0].push([i, 1])}
+            for(let d = monthArr[0].length + 1; d <= 7; d++){
+                monthArr[w][d-1] = [dayCount.toString(), 0]
+                dayCount++
+            }
+        }
+        else {
+            monthArr.push([])
+            let monthFlag = 0
+            for(let d = 1; d <= 7; d++){
+                if(dayCount > daysInMonth(date.getMonth()+1, date.getFullYear())){
+                    dayCount = 1
+                    monthFlag = 1
+                }
+                monthArr[w][d-1] = [dayCount.toString(), monthFlag]
+                dayCount++
+            }
+        }
+    }
+    return monthArr
+}
+
+function monthGenerator(date){
+    document.querySelector('#dateStr').innerHTML = `${monthNames[date.getMonth()]} ${date.getFullYear()}`
+    let monthArr = monthArrayGenerator(date)
+    let dt = document.querySelector('#daysTable')
+    dt.innerHTML = dt.children[0].innerHTML
+    for(let w = 0; w < 5; w++){
+        let r = dt.appendChild(document.createElement('tr'))
+        for(let d = 1; d <= 7; d++){
+            let l = r.appendChild(document.createElement('td'))
+            l.innerText = monthArr[w][d-1][0]
+            if(monthArr[w][d-1][1]){l.classList.add('anotherMonth')}
+        }
+    }
+}
+
+
+let date = new Date()
+monthGenerator(date)
+
+document.querySelector('#monthLeft').addEventListener('click', (e) => {
+    date = new Date(date.getFullYear(), date.getMonth()-1)
+    monthGenerator(date)
+})
+
+document.querySelector('#monthRight').addEventListener('click', (e) => {
+    date = new Date(date.getFullYear(), date.getMonth()+1)
+    monthGenerator(date)
+})
+
